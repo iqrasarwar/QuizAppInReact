91511bbacf3dc8a3b712048eb4d50c6f687fb199
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-20 22:52:19 +0500

    rendering questions and updating state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,61 @@
 import "../styles.css";
 import React from "react";
 
+export default function Question({ questions }) {
+  const [quizz, setQuiz] = React.useState(questions);
+
+  React.useEffect(() => {
+    setQuiz(() => questions);
+  }, [questions]);
+  function selectOption(quesid, option, quizz) {
+    let n;
+    for (let i = 0; i < 4; i++) {
+      if (quizz[quesid].options[i].option === option.target.innerText) n = i;
+    }
+    setQuiz((prevQuiz) => {
+      let x = [...prevQuiz, (prevQuiz[quesid].options[n].isSelected = true)];
+      console.log(x);
+      return x;
+    });
+  }
+  function QuesCom({ ques, index, quizz }) {
+    return (
+      <div className="question">
+        <div>
+          <p className="statment">{ques.question}</p>
+          <div className="options">
+            <p
+              onClick={(e) => selectOption(index, e, quizz)}
+              className={`${
+                ques.options[0].isSelected === true ? "selected" : "notSl"
+              }`}
+            >
+              {ques.options[0].option}
+            </p>
+            <p onClick={(e) => selectOption(index, e, quizz)}>
+              {ques.options[1].option}
+            </p>
+            <p onClick={(e) => selectOption(index, e, quizz)}>
+              {ques.options[2].option}
+            </p>
+            <p onClick={(e) => selectOption(index, e, quizz)}>
+              {ques.options[3].option}
+            </p>
+          </div>
+        </div>
+        <Hr key={index} id={index} />
+      </div>
+    );
+  }
+
+  let questionsArr = quizz.map((ques, index) => (
+    <QuesCom key={index} ques={ques} index={index} quizz={quizz} />
+  ));
+
+  return <div>{questionsArr}</div>;
+}
+
 function Hr(props) {
   if (props.id !== 4) return <hr />;
   else return <> </>;
-}
-export default function Question(props) {
-  return (
-    <div className="question">
-      <p className="statment">{props.question}</p>
-      <div className="options">
-        <p>{props.options[0].option}</p>
-        <p>{props.options[1].option}</p>
-        <p>{props.options[2].option}</p>
-        <p>{props.options[3].option}</p>
-      </div>
-      <Hr id={props.id} />
-    </div>
-  );
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,7 +2,7 @@ import "../styles.css";
 import React from "react";
 import Question from "./Question";
 
-export default function Questions(props) {
+export default function Questions() {
   const [questions, setQuestions] = React.useState([]);
   React.useEffect(() => {
     let url = "https://opentdb.com/api.php?amount=5&category=18&type=multiple";
@@ -10,49 +10,22 @@ export default function Questions(props) {
       .then((response) => response.json())
       .then((data) => {
         setQuestions(data.results);
-        setQuiz(stateArr);
       });
   }, []);
-
   const options = questions.map((ques) =>
     makeOptions(ques.incorrect_answers, ques.correct_answer)
   );
-  let stateArr = [];
-  function stateArray() {
-    questions.forEach((ques, index) => {
-      stateArr.push({
-        question: ques.question,
-        options: options[index],
-        correctOpt: ques.correct_answer
-      });
-    });
-    return stateArr;
-  }
-
-  const [quiz, setQuiz] = React.useState(stateArray());
-  React.useEffect(() => {
-    setQuiz(stateArr);
-  }, []);
-  console.log(quiz);
-  let questionsArr = questions.map((ques, index) => (
-    <Question
-      key={index}
-      question={stateArr[index].question}
-      options={stateArr[index].options}
-      id={index}
-    />
-  ));
+  let quiz = stateArray(questions, options);
 
   return (
     <div>
       <div className="cover--questions">
-        {questionsArr}
+        <Question questions={quiz} />
         <button className="cover--submit quiz--submit">Submit Quiz</button>
       </div>
     </div>
   );
 }
-
 function makeOptions(inc, c) {
   let x = [];
   x.push(...inc);
@@ -64,4 +37,15 @@ function makeOptions(inc, c) {
     };
   });
   return y;
+}
+function stateArray(questions, options) {
+  let stateArr = [];
+  questions.forEach((ques, index) => {
+    stateArr.push({
+      question: ques.question,
+      options: options[index],
+      correctOpt: ques.correct_answer
+    });
+  });
+  return stateArr;
 }
