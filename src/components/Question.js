500dfeec812522f0caf5ad2e87e0be9e67b1cdc1
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-24 16:33:12 +0500

    first working version

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,7 @@
 import "../styles.css";
 import React from "react";
 
-export default function Question({ questions }) {
+export default function Question({ questions, resetQuiz }) {
   const [quizz, setQuiz] = React.useState(questions);
 
   React.useEffect(() => {
@@ -11,34 +11,54 @@ export default function Question({ questions }) {
     let n;
     for (let i = 0; i < 4; i++) {
       if (quizz[quesid].options[i].option === option.target.innerText) n = i;
+      else {
+        setQuiz(() => [
+          ...questions,
+          (questions[quesid].options[i].isSelected = false)
+        ]);
+      }
     }
-    setQuiz((prevQuiz) => {
-      let x = [...prevQuiz, (prevQuiz[quesid].options[n].isSelected = true)];
-      console.log(x);
-      return x;
-    });
+    setQuiz(() => [
+      ...questions,
+      (questions[quesid].options[n].isSelected = true)
+    ]);
   }
-  function QuesCom({ ques, index, quizz }) {
+  function handleClicks(index, e, questions) {
+    if (formSubmitted === true) return false;
+    selectOption(index, e, questions);
+  }
+  function QuesCom({ ques, index }) {
     return (
       <div className="question">
         <div>
           <p className="statment">{ques.question}</p>
           <div className="options">
             <p
-              onClick={(e) => selectOption(index, e, quizz)}
-              className={`${
-                ques.options[0].isSelected === true ? "selected" : "notSl"
-              }`}
+              onClick={(e) => handleClicks(index, e, questions)}
+              className={selectClass(ques, 0)}
+              id={selectId(ques.options[0].markedCorrect)}
             >
               {ques.options[0].option}
             </p>
-            <p onClick={(e) => selectOption(index, e, quizz)}>
+            <p
+              onClick={(e) => handleClicks(index, e, questions)}
+              className={selectClass(ques, 1)}
+              id={selectId(ques.options[1].markedCorrect)}
+            >
               {ques.options[1].option}
             </p>
-            <p onClick={(e) => selectOption(index, e, quizz)}>
+            <p
+              onClick={(e) => handleClicks(index, e, questions)}
+              className={selectClass(ques, 2)}
+              id={selectId(ques.options[2].markedCorrect)}
+            >
               {ques.options[2].option}
             </p>
-            <p onClick={(e) => selectOption(index, e, quizz)}>
+            <p
+              onClick={(e) => handleClicks(index, e, questions)}
+              className={selectClass(ques, 3)}
+              id={selectId(ques.options[3].markedCorrect)}
+            >
               {ques.options[3].option}
             </p>
           </div>
@@ -47,15 +67,69 @@ export default function Question({ questions }) {
       </div>
     );
   }
-
-  let questionsArr = quizz.map((ques, index) => (
+  let questionsArr = questions.map((ques, index) => (
     <QuesCom key={index} ques={ques} index={index} quizz={quizz} />
   ));
 
-  return <div>{questionsArr}</div>;
+  return (
+    <div>
+      {questionsArr}
+      {formSubmitted === false ? (
+        <button
+          className="cover--submit quiz--submit"
+          onClick={() => submitQuiz(questions)}
+        >
+          Submit Quiz
+        </button>
+      ) : (
+        <button className="cover--submit quiz--submit" onClick={resetQuizz}>
+          Reset Quiz
+        </button>
+      )}
+    </div>
+  );
+  function resetQuizz() {
+    formSubmitted = false;
+    resetQuiz();
+  }
+
+  function selectClass(opt, index) {
+    if (opt.options[index].isSelected) return "selected";
+    if (formSubmitted && opt.options[index].option === opt.correctOpt)
+      return "markcorrect";
+  }
+  function selectId(val) {
+    if (val === 1) return "correct";
+    else if (val === 0) return "incorrect";
+    else return "";
+  }
+  function submitQuiz(questions) {
+    formSubmitted = true;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (questions[i].options[j].isSelected) {
+          if (questions[i].options[j].option === questions[i].correctOpt) {
+            count++;
+            setQuiz(() => [
+              ...questions,
+              (questions[i].options[j].markedCorrect = 1)
+            ]);
+          } else {
+            setQuiz(() => [
+              ...questions,
+              (questions[i].options[j].markedCorrect = 0)
+            ]);
+          }
+        }
+      }
+    }
+    if (count === 0) setQuiz(() => [...questions]);
+  }
 }
 
 function Hr(props) {
   if (props.id !== 4) return <hr />;
   else return <> </>;
 }
+let formSubmitted = false;
+let count = 0;
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,7 +2,7 @@ import "../styles.css";
 import React from "react";
 import Question from "./Question";
 
-export default function Questions() {
+export default function Questions({ handleClick, quizState }) {
   const [questions, setQuestions] = React.useState([]);
   React.useEffect(() => {
     let url = "https://opentdb.com/api.php?amount=5&category=18&type=multiple";
@@ -11,29 +11,29 @@ export default function Questions() {
       .then((data) => {
         setQuestions(data.results);
       });
-  }, []);
+  }, [quizState]);
   const options = questions.map((ques) =>
     makeOptions(ques.incorrect_answers, ques.correct_answer)
   );
   let quiz = stateArray(questions, options);
-
   return (
     <div>
       <div className="cover--questions">
-        <Question questions={quiz} />
-        <button className="cover--submit quiz--submit">Submit Quiz</button>
+        <Question questions={quiz} resetQuiz={handleClick} />
       </div>
     </div>
   );
 }
 function makeOptions(inc, c) {
   let x = [];
+  let randomNum = Math.floor(Math.random() * 4);
   x.push(...inc);
-  x.push(c);
+  x.splice(randomNum, 0, c);
   let y = x.map((opt) => {
     return {
       option: opt,
-      isSelected: false
+      isSelected: false,
+      markedCorrect: 2
     };
   });
   return y;
