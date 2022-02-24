4f489b9f42421bddfc6123a20bf1dae4a55a8646
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-25 00:09:24 +0500

    box shadow and loading icon

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,16 +1,38 @@
 import "../styles.css";
 import React from "react";
 import Question from "./Question";
-
+import { trackPromise } from "react-promise-tracker";
+import { usePromiseTracker } from "react-promise-tracker";
+import { Bars } from "react-loading-icons";
+const LoadingIndicator = (props) => {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    promiseInProgress && (
+      <div
+        style={{
+          width: "100%",
+          height: "100",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Bars stroke="#00465c" />
+      </div>
+    )
+  );
+};
 export default function Questions({ handleClick, quizState }) {
   const [questions, setQuestions] = React.useState([]);
   React.useEffect(() => {
     let url = "https://opentdb.com/api.php?amount=5&category=18&type=multiple";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setQuestions(data.results);
-      });
+    trackPromise(
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setQuestions(data.results);
+        })
+    );
   }, [quizState]);
   const options = questions.map((ques) =>
     makeOptions(ques.incorrect_answers, ques.correct_answer)
@@ -19,6 +41,7 @@ export default function Questions({ handleClick, quizState }) {
   return (
     <div>
       <div className="cover--questions">
+        <LoadingIndicator />
         <Question questions={quiz} resetQuiz={handleClick} />
       </div>
     </div>
